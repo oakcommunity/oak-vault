2ff43061b79d4d7c9f164ede97cd753a932a4e98
taayyohh <milesxbridges@gmail.com>
2023-09-06 22:09:13 -0700

    init tests

--- src/OakVault.sol
+++ src/OakVault.sol
@@ -1,22 +1,143 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import { Initializable } from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import { IERC20Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+import { PausableUpgradeable } from "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import { OwnableUpgradeable } from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import { ReentrancyGuardUpgradeable } from "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+import { SafeERC20Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
 
-contract OakVault is Initializable, PausableUpgradeable, OwnableUpgradeable {
+/**
+ * @title OakVault Contract
+ * @author taayyohh
+ * @notice A contract to manage swaps between USDC and $OAK tokens.
+ */
+contract OakVault is Initializable, PausableUpgradeable, OwnableUpgradeable, ReentrancyGuardUpgradeable {
+    using SafeERC20Upgradeable for IERC20Upgradeable;
+
+    // State variables
     IERC20Upgradeable public oakToken;
     IERC20Upgradeable public usdcToken;
     mapping(address => uint256) public lastSwapTime;
 
+    // Constants
     uint256 public constant SWAP_LIMIT = 10 * 10**6; // 10 USDC
     uint256 public constant TIME_LIMIT = 1 days;
 
-    // Constructor logic will go here
+    // Custom errors
+    error InsufficientTokenBalance();
+    error InsufficientUSDCBalance();
+    error ExceedsSwapLimit();
+    error SwapCooldown();
+    error InvalidUSDCAddress();
+    error InvalidOakAddress();
 
-    // Modifiers will go here
+    // Events
+    event SwappedUSDCForOak(address indexed user, uint256 amount);
+    event SwappedOakForUSDC(address indexed user, uint256 amount, uint256 surcharge);
+    event USDCWithdrawn(address indexed owner, uint256 amount);
+    event USDCDeposited(address indexed owner, uint256 amount);
+    event OakDeposited(address indexed owner, uint256 amount);
 
-    // Functions will go here
+
+    /**
+     * @notice Initializes the contract with the addresses of $OAK and USDC tokens.
+     * @param _oakToken The address of the $OAK token.
+     * @param _usdcToken The address of the USDC token.
+     */
+    function initialize(address _oakToken, address _usdcToken) public initializer {
+        oakToken = IERC20Upgradeable(_oakToken);
+        usdcToken = IERC20Upgradeable(_usdcToken);
+
+        __Pausable_init();
+        __Ownable_init();
+    }
+
+    /**
+     * @notice Modifier to check if a user can perform a swap.
+     * @param user The address of the user.
+     * @param amount The amount of tokens to swap.
+     */
+    modifier canSwap(address user, uint256 amount) {
+        if (amount > SWAP_LIMIT) revert ExceedsSwapLimit();
+        if (block.timestamp - lastSwapTime[user] < TIME_LIMIT) revert SwapCooldown();
+        _;
+    }
+
+    /**
+     * @notice Allows a user to swap USDC for $OAK.
+     * @param amount The amount of USDC to swap.
+     */
+    function swapUSDCForOak(uint256 amount) external canSwap(msg.sender, amount) whenNotPaused nonReentrant {
+        if (oakToken.balanceOf(address(this)) < amount) revert InsufficientTokenBalance();
+
+        // Transfer USDC from user to contract
+        usdcToken.safeTransferFrom(msg.sender, address(this), amount);
+
+        // Transfer equivalent $OAK to user
+        oakToken.safeTransfer(msg.sender, amount);
+
+        // Update last swap time
+        lastSwapTime[msg.sender] = block.timestamp;
+
+        emit SwappedUSDCForOak(msg.sender, amount);
+    }
+
+    /**
+     * @notice Allows a user to swap $OAK for USDC with a surcharge.
+     * @param amount The amount of $OAK to swap.
+     */
+    function swapOakForUSDC(uint256 amount) external whenNotPaused nonReentrant {
+        uint256 surcharge = (amount * 5) / 100; // 5% surcharge
+        uint256 netAmount = amount - surcharge;
+
+        if (usdcToken.balanceOf(address(this)) < netAmount) revert InsufficientUSDCBalance();
+
+        // Transfer $OAK from user to contract
+        oakToken.safeTransferFrom(msg.sender, address(this), amount);
+
+        // Transfer USDC after deducting surcharge to user
+        usdcToken.safeTransfer(msg.sender, netAmount);
+
+        emit SwappedOakForUSDC(msg.sender, amount, surcharge);
+    }
+
+    /**
+     * @notice Allows the owner to withdraw USDC from the contract.
+     * @param amount The amount of USDC to withdraw.
+     */
+    function withdrawUSDC(uint256 amount) external onlyOwner {
+        if (usdcToken.balanceOf(address(this)) < amount) revert InsufficientUSDCBalance();
+
+        usdcToken.transfer(msg.sender, amount);
+
+        emit USDCWithdrawn(msg.sender, amount);
+    }
+
+    /**
+   * @notice Allows the owner to deposit USDC into the contract.
+     * @param tokenAddress The address of the USDC token being deposited.
+     * @param amount The amount of USDC to deposit.
+     */
+    function depositUSDC(address tokenAddress, uint256 amount) external onlyOwner {
+        if (tokenAddress != address(usdcToken)) revert InvalidUSDCAddress();
+
+        // Transfer USDC from owner to contract
+        usdcToken.safeTransferFrom(msg.sender, address(this), amount);
+        emit USDCDeposited(msg.sender, amount);
+    }
+
+    /**
+     * @notice Allows the owner to deposit $OAK tokens into the contract.
+     * @param tokenAddress The address of the $OAK token being deposited.
+     * @param amount The amount of $OAK tokens to deposit.
+     */
+    function depositOak(address tokenAddress, uint256 amount) external onlyOwner {
+        if (tokenAddress != address(oakToken)) revert InvalidOakAddress();
+
+        // Transfer $OAK tokens from owner to contract
+        oakToken.safeTransferFrom(msg.sender, address(this), amount);
+        emit OakDeposited(msg.sender, amount);
+    }
 }
--- test/OakVault.t.sol
+++ test/OakVault.t.sol
@@ -1,24 +1,129 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity 0.8.10;
 
 import "forge-std/Test.sol";
-import "../src/OakVault.sol";
+import { IERC20Upgradeable } from "openzeppelin-contracts-upgradeable/contracts/token/ERC20/IERC20Upgradeable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract CounterTest is Test {
-    Counter public counter;
+import { OakVault } from "../src/OakVault.sol";
+
+
+interface IUSDC {
+    function masterMinter() external view returns (address);
+    function configureMinter(address minter, uint256 minterAllowedAmount) external;
+    function mint(address account, uint256 amount) external;
+}
+
+contract OakVaultTest is Test {
+    OakVault oakVault;
+    IERC20Upgradeable oakToken;
+    IERC20Upgradeable usdcToken;
+
+    address deployer;
+    address user1;
+    address user2;
 
     function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
+        deployer = address(this); // The test contract is the deployer
+        user1 = address(0x123); // Replace with actual test address
+        user2 = address(0x456); // Replace with actual test address
+
+        oakToken = IERC20Upgradeable(address(0xabc)); // Mock or actual token address
+        usdcToken = IERC20Upgradeable(address(0xdef)); // Mock or actual token address
+
+        // Prank as user1 to deploy the contract
+        vm.startPrank(user1);
+        oakVault = new OakVault();
+        oakVault.initialize(address(oakToken), address(usdcToken));
+
+        // Mint USDC and OAK tokens to user1
+        IUSDC(address(usdcToken)).mint(user1, 10000e18);
+        IUSDC(address(oakToken)).mint(user1, 10000e18); // Assuming OAK token has a similar mint function
+
+        // User1 approves the OakVault contract to spend tokens
+        oakToken.approve(address(oakVault), 10000e18);
+        usdcToken.approve(address(oakVault), 10000e18);
+//
+//        // User1 deposits USDC and OAK tokens into the contract
+//        oakVault.depositUSDC(user1, 10000e18);
+        oakVault.depositOak(user1, 10000e18);
     }
 
-    function testIncrement() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
+    function test_InitializeOakVault() public {
+        assertEq(address(oakVault.oakToken()), address(oakToken));
+        assertEq(address(oakVault.usdcToken()), address(usdcToken));
     }
 
-    function testSetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+    function test_SwapUSDCForOak() public {
+        uint256 amount = 5 * 10**6; // 5 USDC
+
+        // Deal USDC to user1
+        deal(address(usdcToken), user1, amount);
+
+        // Prank as user1
+        vm.startPrank(user1);
+
+        oakVault.swapUSDCForOak(amount);
+
+        // Check the balances and lastSwapTime
+        assertEq(usdcToken.balanceOf(address(oakVault)), amount);
+        assertEq(oakToken.balanceOf(user1), amount);
+        assertEq(oakVault.lastSwapTime(user1), block.timestamp);
+    }
+
+    function testFail_SwapUSDCAboveLimit() public {
+        uint256 amount = 11 * 10**6; // 11 USDC, which is above the limit
+
+        // Deal USDC to user1
+        deal(address(usdcToken), user1, amount);
+
+        // Prank as user1
+        vm.startPrank(user1);
+
+        oakVault.swapUSDCForOak(amount);
+    }
+
+    function test_SwapOakForUSDC() public {
+        uint256 amount = 5 * 10**6; // 5 OAK
+        uint256 surcharge = (amount * 5) / 100; // 5% surcharge
+        uint256 netAmount = amount - surcharge;
+
+        // Deal OAK to user2
+        deal(address(oakToken), user2, amount);
+
+        // Prank as user2
+        vm.startPrank(user2);
+
+        oakVault.swapOakForUSDC(amount);
+
+        // Check the balances
+        assertEq(oakToken.balanceOf(address(oakVault)), amount);
+        assertEq(usdcToken.balanceOf(user2), netAmount);
+    }
+
+    function test_WithdrawUSDCByOwner() public {
+        uint256 amount = 5 * 10**6; // 5 USDC
+
+        // Deal USDC to the contract
+        deal(address(usdcToken), address(oakVault), amount);
+
+        // Prank as deployer
+        vm.startPrank(deployer);
+
+        oakVault.withdrawUSDC(amount);
+
+        // Check the balance
+        assertEq(usdcToken.balanceOf(deployer), amount);
+    }
+
+    function testFail_WithdrawUSDCByNonOwner() public {
+        uint256 amount = 5 * 10**6; // 5 USDC
+
+        // Deal USDC to the contract
+        deal(address(usdcToken), address(oakVault), amount);
+
+        // Prank as user1
+        vm.startPrank(user1);
+
+        oakVault.withdrawUSDC(amount);
     }
 }
